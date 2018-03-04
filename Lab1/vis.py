3803ee09fded6664bdd116908fd0122be7048a3a
JessikaSmith <mariyaxod@yandex.ru>
2018-03-04 13:15:20 +0300

    male/fem graph added

--- Lab1/vis.py
+++ Lab1/vis.py
@@ -11,11 +11,6 @@ group = ['0-4', '5-9', '10-14', '15-19', '20-24',
          '80-84', '85-89', '90-94', '95-99', '100']
 
 
-def profiles_m_f(fem_data, male_data, year, title):
-
-    return True
-
-
 def show_profile(data, year, type, title):
 
     y = [y*1000 for y in data[data['date'] == year][group].values.tolist()[0]]
@@ -25,7 +20,8 @@ def show_profile(data, year, type, title):
         mode='lines+markers',
         name='title',
         line=dict(
-            color=('rgb(205, 12, 24)'))
+            color=('rgb(205, 12, 24)')
+        )
     )
     layout = dict(title='Demographic profile prediction for Russia in '+str(year) + ' (' + type + ')',
                   xaxis=dict(title='Age group'),
@@ -52,4 +48,27 @@ def profile_compare_years(data, years, type, title):
                   yaxis=dict(title='Amount of people'),
                   )
     fig = go.Figure(data=traces, layout=layout)
+    image.save_as(fig, filename=path_to_figure + title + '.jpeg')
+
+
+def profiles_m_f(fem_data, male_data, year, title):
+
+    y = [y * 1000 for y in fem_data[fem_data['date'] == year][group].values.tolist()[0]]
+    trace1 = go.Scatter(
+        x=group,
+        y=y,
+        mode='lines+markers',
+        name='female')
+    y = [y * 1000 for y in fem_data[male_data['date'] == year][group].values.tolist()[0]]
+    trace2 = go.Scatter(
+        x=group,
+        y=y,
+        mode='lines+markers',
+        name='male')
+    layout = dict(title='Demographic profile prediction for Russia in ' + str(year),
+                  xaxis=dict(title='Age group'),
+                  yaxis=dict(title='Amount of people'),
+                  )
+    data = [trace1, trace2]
+    fig = go.Figure(data=data, layout=layout)
     image.save_as(fig, filename=path_to_figure + title + '.jpeg')
