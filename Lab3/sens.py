2245107ab54c01d3db68df852e0ff9ea38781d3b
J3FALL <crisp.coad@gmail.com>
2018-06-06 00:43:45 +0300

    Fix path to excel file

--- Lab3/sens.py
+++ Lab3/sens.py
@@ -1,8 +1,7 @@
-from SALib.sample.saltelli import sample
-from SALib.analyze import sobol
-from Lab1.prediction_model import PredictionModel
-
 import numpy as np
+from SALib.sample.saltelli import sample
+
+from Lab1.prediction_model import PredictionModel
 
 
 def model(x1, x2, x3):
@@ -11,11 +10,12 @@ def model(x1, x2, x3):
 
 def eval(param_values):
     predictions = list()
-    name = 'D:\\UNIVERSITY\\MS\\Semester2\\Descrete Mathematical Models\\Code\\DMM\\Lab1\\age_data.xls'
+    name = '../Lab1/age_data.xls'
     fem_sheet = 'f; 1950-2005, estimates'
     male_sheet = 'm; 1950-2005, estimates'
     both_sheet = 'both; 1950-2005, estimates'
     for params in param_values:
+        print(params)
         fertility, babies_fraction = params
         model = PredictionModel(name, fem_sheet, male_sheet, both_sheet, babies_fraction)
         predicted = model.pred_model_1_year_with_fertility(101, fertility)
